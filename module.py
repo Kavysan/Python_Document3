85b15e30549c34a3a5325a8c9f1e4301ccbc0064
Kavya Santhakumar <kavysan@Kavysan-MacBook-Pro-2.local>
2023-08-26 08:23:51 -0400

    first commit

---
+++ module.py
@@ -0,0 +1,10 @@
+import math
+def printName(name):
+    print("Hello "+name)
+def area():
+    l=float(input("What is the length? "))
+    w=float(input("What is the width? "))
+    return l*w 
+def circum():
+    r=float(input("What is the radius? "))
+    return 2*math.pi*r
